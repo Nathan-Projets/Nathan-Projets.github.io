68d02dc6868e0ffc3d3c74af20df22a5b074d286
nathan-projets <guillouxnathan22@gmail.com>
2020-11-10 15:12:03 +0100

    Add TP2 JS

--- projets/js/tps/tp2/assets/js/index.js
+++ projets/js/tps/tp2/assets/js/index.js
@@ -1,15 +1,23 @@
 var compteurElem = document.getElementById("compteur");
+var image = document.getElementById("image");
 var i = 10;
 
 function decremente() {
-    console.log(i);
     if(i > 0) {
         i -= 1;
         compteurElem.innerText = i;
     } else {
         clearInterval(timer);
+        image.src = "assets/image/update-arrow.svg";
+        image.addEventListener("click", function() {
+            clearInterval(timer);
+            image.src = "assets/image/stopwatch.svg";
+            i = 10;
+            compteurElem.innerText = i;
+            timer = setInterval('decremente()', 1000);
+            this.removeEventListener("click", function(){});
+        });
     }
-    
 }
 
-var timer = setInterval('decremente()', 1000);
+var timer = setInterval(decremente, 1000);
